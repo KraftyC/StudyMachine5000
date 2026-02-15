f90e19444825a163b87c5c4156e6e87b32e4d008
Mike Zammit <mike.r.zammit@gmail.com>
2026-02-14 22:11:32 -0500

    Added AI LD & FH Midterm Questions

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,10 +1,15 @@
 export const CHAPTER_GROUPS = {  // { CourseCode: { GroupName: { Textbook: [ Chapter1, Chapter2, ... ] }}}
-  ANAT9240: {
-    "Test #1": { 
-      "Anatomy & Physiology": [ "10 - Skin", "11 - Skeletal Tissues", "12 - Axial Skeleton", "13 - Appendicular Skeleton", "14 - Articulations", "15 - Axial Muscles", "16 - Appendicular Muscles", "17 - Muscle Contraction" ] 
+  ANAT9240: null,
+  PSYC9210: {
+    "Midterm Exam": { 
+      "Developmental Psychology: A Canadian Perspective": [ "1 - Introduction to Lifespan Development", "2 - Heredity, Prenatal Development, and Birth", "3 - Infancy and Toddlerhood", "4 - Early Childhood" ] 
     } 
   },
-  PSYC9210: null,
-  BSCN9200: null,
+  BSCN9200: {
+    "Midterm Exam": { 
+      "Canadian Fundamentals of Nursing": [ "20 - Family Health" ],
+      "Leifer's Introduction to Maternity & Pediatric Nursing": [ "1 - Overview of Perinatal & Pediatric Nursing in Canada", "2 - The Nurse's Role in Reproductive Health Care", "3 - Fetal Development", "4 - Prenatal Care and Adaptations to Pregnancy", "5 - Nursing Care of Patients with Complications During Pregnancy", "6 - Nursing Care of the Family During Labour and Birth", "7 - Pain Management During Labour and Birth", "8 - Nursing Care of Patients With Complications During Labour and Birth", "9 - Nursing Care of the Postpartum Person and Family After Birth", "10 - Nursing Care of Patients With Complications After Birth", "11 - The Term Newborn", "14 - The Infant" ]
+    } 
+  },
   BSCN9230: null
 }
