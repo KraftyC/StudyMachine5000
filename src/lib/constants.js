a6432190b7c61239e3c4f5047ad7d8c85c826e19
Mike Zammit <mike.r.zammit@gmail.com>
2026-02-17 22:46:41 -0500

    Added SC Questions

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -11,5 +11,10 @@ export const CHAPTER_GROUPS = {  // { CourseCode: { GroupName: { Textbook: [ Cha
       "Leifer's Introduction to Maternity & Pediatric Nursing": [ "1 - Overview of Perinatal & Pediatric Nursing in Canada", "2 - The Nurse's Role in Reproductive Health Care", "3 - Fetal Development", "4 - Prenatal Care and Adaptations to Pregnancy", "5 - Nursing Care of Patients with Complications During Pregnancy", "6 - Nursing Care of the Family During Labour and Birth", "7 - Pain Management During Labour and Birth", "8 - Nursing Care of Patients With Complications During Labour and Birth", "9 - Nursing Care of the Postpartum Person and Family After Birth", "10 - Nursing Care of Patients With Complications After Birth", "11 - The Term Newborn", "14 - The Infant" ]
     } 
   },
-  BSCN9230: null
+  BSCN9230: {
+    "Midterm & Final Exams": { 
+      "Canadian Clinical Nursing Skills & Techniques (2nd Ed.)": [ "3 - Communication and Collaboration", "4 - Documentation and Informatics", "5 - Medical Asepsis", "7 - Vital Signs", "11 - Safe Patient Handling, Transfer, & Positioning", "12 - Exercise, Mobility, & Immobilization Devices", "16 - Pain Assessment and Management", "18 - Personal Hygiene & Bed Making", "31 - Oral Nutrition", "34 - Urinary Elimination and Catheterization", "35 - Bowel Elimination and Gastric Intubation", "39 - Prevention & Care of Skin Breakdown" ],
+      "Medical Terminology": [ "Week 2", "Week 3", "Week 4", "Week 5", "Week 6" ]
+    } 
+  }
 }
